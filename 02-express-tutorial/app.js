9f98087e06b439db6ccde1189a443cd62dfee520
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-10-22 11:34:41 +0700

    update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const people = require('./routes/people');
+const auth = require('./routes/auth');
+
+app.use(express.static('./methods-public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api/people', people);
+app.use('/login', auth);
+
+app.listen(5000, () => {
+  console.log('Sever is listening on port 5000...');
+});
---
+++ 02-express-tutorial/controller/people.js
@@ -0,0 +1,65 @@
+const nodemon = require('nodemon');
+let { people } = require('../data');
+
+const getPeople = (req, res) => {
+  res.status(200).json({ success: true, data: people });
+};
+
+const createPerson = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(201)
+      .json({ success: false, msg: 'please provide a name' });
+  }
+  res.status(201).send({ success: true, person: name });
+};
+
+const createPersonPostman = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(201)
+      .json({ success: false, msg: 'please provide a name' });
+  }
+  res.status(201).send({ success: true, data: [...people, name] });
+};
+
+const updatePerson = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.id = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+};
+
+const deletePerson = (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  res.status(200).json({ success: true, data: newPeople });
+};
+
+module.exports = {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+};
